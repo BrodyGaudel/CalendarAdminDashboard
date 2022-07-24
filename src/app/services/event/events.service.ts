d417b71236c4d9cb32653391d950fe636ffe31c4
Brody Gaudel <brodymounanga@gmail.com>
2022-07-24 03:57:00 +0100

    fix bug

--- src/app/services/event/events.service.ts
+++ src/app/services/event/events.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {AuthService} from "../auth/auth.service";
 import {Eventmodel} from "../../models/eventmodel.model";
 import {EventFormAdd} from "../../models/event.model";
@@ -10,6 +10,14 @@ import {EventFormAdd} from "../../models/event.model";
 export class EventsService {
 
   constructor(private http: HttpClient, private authService: AuthService) { }
+
+  jwtSecurity(): HttpHeaders{
+    let jwt = this.authService.getToken();
+    jwt = 'Bearer ' + jwt;
+    const httpHeaders = new HttpHeaders({Authorization: jwt});
+    return httpHeaders;
+  }
+
 
   getEvents(idCalendar: string){
 
